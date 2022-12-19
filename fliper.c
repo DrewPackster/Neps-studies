f14fdd62f7edf7f92ba4936cdd93a004af1e85d2
Andrew Douglas <120404716+DrewPackster@users.noreply.github.com>
2022-12-19 10:11:24 -0300

    Add files via upload

---
+++ fliper.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+    int P,R;
+
+    printf("insira 0 ou 1 para P");
+    scanf("%d\n", &P);
+if(P==0){
+
+printf("C");
+}
+
+if(P==1){
+
+    printf("insira 0 ou 1 para R");
+}
+    scanf("%d\n", &R);
+if(R==0){
+    printf("B");
+}
+
+if(R==1){
+    printf("A");
+}
+
+}
